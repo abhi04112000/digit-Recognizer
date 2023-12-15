56f74576cf1d1553766ff6e1847b9206594bf2dc
Abhinav Pratap Singh <140481919+abhi04112000@users.noreply.github.com>
2023-12-15 14:51:06 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,12 +1,12 @@
 # app.py
 import streamlit as st
 from streamlit_drawable_canvas import st_canvas
+from PIL import Image
 import tensorflow as tf
 from tensorflow.keras import models
 import numpy as np
-from PIL import Image
 
-# Load the trained model
+# Load the trained model outside the main script
 model = models.load_model('mnist_model.h5')
 
 # Function to preprocess an image
@@ -25,6 +25,17 @@ def preprocess_drawn_image(drawn_image):
     # Convert to array and reshape
     img_array = np.array(img).reshape((1, 28, 28, 1)).astype('float32') / 255.0
     return img_array
+
+# Function to get canvas for drawing
+def get_drawing_canvas():
+    return st_canvas(
+        fill_color="rgba(255, 255, 255, 0.1)",  # Background color of the canvas
+        stroke_width=10,
+        stroke_color="#000",
+        update_streamlit=True,
+        height=150,
+        drawing_mode="freedraw",
+    )
 
 # Set page configuration
 st.set_page_config(
@@ -58,42 +69,47 @@ if input_type == "Upload Image":
         col1.image(uploaded_file, caption="Uploaded Image.", use_column_width=True)
 
         # Predict button
-        if col1.button("Predict"):
+        if st.button("Predict"):
             # Preprocess the uploaded image and make predictions
             image_array = preprocess_image(uploaded_file)
             prediction = model.predict(image_array)
             predicted_label = np.argmax(prediction)
 
-            # Display the prediction
+            # Display the prediction and input image
             col1.write("Prediction:")
             col1.write(f"Predicted Digit: {predicted_label}")
             col1.write("Prediction Probabilities:")
             col1.bar_chart(prediction.flatten())
 
+            # Display the input image for reference
+            col1.image(image_array[0, :, :, 0], caption="Input Image", use_column_width=True, cmap="gray")
+
 elif input_type == "Draw Digit":
     # Create a canvas for drawing
-    canvas_result = st_canvas(
-        fill_color="rgba(255, 255, 255, 0.1)",  # Background color of the canvas
-        stroke_width=10,
-        stroke_color="#000",
-        update_streamlit=True,
-        height=150,
-        drawing_mode="freedraw",
-    )
+    canvas_result = get_drawing_canvas()
+
+    # Debugging: Print canvas image_data
+    st.write("Canvas Image Data:", canvas_result.image_data)
 
     # Predict button for drawing
-    if col1.button("Predict"):
+    if st.button("Predict"):
         if canvas_result.image_data is not None:
+            # Debugging: Print shape of the drawn image
+            st.write("Drawn Image Shape:", canvas_result.image_data.shape)
+
             # Preprocess the drawn image and make predictions
             drawn_image_array = preprocess_drawn_image(canvas_result.image_data)
             prediction = model.predict(drawn_image_array)
             predicted_label = np.argmax(prediction)
 
-            # Display the prediction
+            # Display the prediction and input image
             col1.write("Prediction:")
             col1.write(f"Predicted Digit: {predicted_label}")
             col1.write("Prediction Probabilities:")
             col1.bar_chart(prediction.flatten())
+
+            # Display the input image for reference
+            col1.image(drawn_image_array[0, :, :, 0], caption="Input Image", use_column_width=True, cmap="gray")
 
 # Instructions and model summary
 st.sidebar.header("How to Use")
@@ -103,5 +119,3 @@ st.sidebar.write(
     "3. Press the 'Predict' button to make predictions.\n"
     "4. View the predicted digit and probabilities."
 )
-
-# Streamlit app run command: streamlit run app.py
